a6265688f3b362714ad4339ff9448d249c41e6ff
Drincann <1019933576@qq.com>
2022-03-18 02:58:06 +0800

    feature: 添加手动测试用账号

--- db/testingdata.sql
+++ db/testingdata.sql
@@ -8,6 +8,21 @@ insert into
   `Patient`(username, password, situation, injected)
 VALUES('patient1', 'patient1', '', 0);
 insert into
+  `Staff`(username, password, type)
+VALUES('admin1', 'admin1', 0);
+insert into
+  `Staff`(username, password, type)
+VALUES('front1', 'front1', 1);
+insert into
+  `Staff`(username, password, type)
+VALUES('doctor1', 'doctor1', 2);
+insert into
+  `Staff`(username, password, type)
+VALUES('nurse1', 'nurse1', 3);
+insert INTO
+  `Staff`(username, password, type)
+VALUES('buyer1', 'buyer1', 4);
+insert into
   Info (name)
 values
   ('某医院');
