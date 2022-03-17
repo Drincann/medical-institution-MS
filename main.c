c3a2e123dd4caefb5e391c8b4bb375006387fcbf
Drincann <1019933576@qq.com>
2022-03-18 03:00:31 +0800

    feature: 实现了程序的主控制流

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include "./db.c"
 #include "./include/mysql.h"
+#include "./menu.c"
+
+char input[256] = {0};
+char* getInput() {
+    scanf("%s", input);
+    return input;
+};
 
 int main() {
     initDB();
+    Menu* menu = &topMenu;
+    while (1) {
+        menu->showMenu();
+
+        string input = getInput();
+        menu = menu->exec(input);
+    }
+
     return EXIT_SUCCESS;
 }
